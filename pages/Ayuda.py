6be17e81e36a94224da7cd9320d7fa719a0bf736
Jaguz78 <jalejandrogh78@gmail.com>
2023-08-24 18:06:05 -0600

    Todo listo

--- pages/Ayuda.py
+++ pages/Ayuda.py
@@ -9,5 +9,5 @@ class AyudaForm(tk.Frame):
 
         e = tk.Label(self, text="SISTEMA DE FACTURACIÓN GG", font=(24)).grid(row=0, pady=15, padx=295)
         tk.Label(self, text="Edgar Anthony Enmanuel Gonzalez López - 202146223").grid(row=1)
-        tk.Label(self, text="José Alejandro Guzmán Heinemann - 202146223").grid(row=2)
+        tk.Label(self, text="José Alejandro Guzmán Heinemann - 202140561").grid(row=2)
         tk.Label(self, text="Archivos - Sexto Semestre 2023").grid(row=3)
